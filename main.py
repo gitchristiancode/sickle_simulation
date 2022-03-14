e00bedd9a8d8dad93d1767ced0ba2f7ac961a373
ChristiamCode <chriscodr11@gmail.com>
2022-03-14 00:32:46 -0400

    Final Simulation

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
 import random
 
+total = 500
+SCT = 0.1
+SCD = 0.0
+
+set = []
+pairs = []
+
+def pairSet():
+    random.shuffle(set)
+    global pairs
+    pairs = []
+
+    for i in range(0,len(set),2):
+        if i+1 < len(set):
+            pairs.append([set[i], set[i+1]])
+
+
+
 class Person:
     def __init__(self, a, b):
         self.chrom = [a, b]
-    
-    def combine(self, a):
+        self.a = 0
+
+    def combine(self, other):
+        self.a += 1
+        other.a += 1
+
         a = random.choice(self.chrom)
-        b = random.choice(self.chrom)
+        b = random.choice(other.chrom)
 
         return Person(a, b)
 
-affected = 0
-carrier = 0
-total = 10000
 
-for i in range(0,total):
-    a = Person(True, False)
-    b = Person(True, False)
-    res = a.combine(b)
+for _ in range(0,int(total*SCT)):
+    a = Person(True,False)
+    set.append(a)
 
-    if(res.chrom.count(True) == 2):
-        affected += 1
+for _ in range(0,int(total*SCD)):
+    a = Person(True,True)
+    set.append(a)
 
-    if(res.chrom.count(True) == 1):
-        carrier += 1
+for _ in range(0,int(total - total*SCD - total*SCT)):
+    a = Person(False,False)
+    set.append(a)
 
-print("aff\tcarrier")
-print(affected,"\t",carrier)
+for generation in range(0,100):
+    newSet = []
+    pairSet()
+    for pair in pairs:
+        for i in range(0,random.randint(1,3)):
+            newSet.append(pair[0].combine(pair[1]))
+    
+    set = newSet
+
+    SCT = len([person for person in set if person.chrom.count(True) == 1])
+    SCD = len([person for person in set if person.chrom.count(True) == 2])
+
+    print(generation, len(set), SCT, SCD, sep="\t")
